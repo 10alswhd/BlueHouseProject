99b1bab8e8d3bee5975e9d81174f61cdd2062695
jimin1992 <korea4647@naver.com>
2016-09-20 13:08:46 +0900

    Create  selectionSort.cpp

---
+++ selectionSort.cpp
@@ -0,0 +1,20 @@
+void swap(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void selectionSort(int a[], int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a[i] > a[j])
+			{
+				swap(a[i], a[j]);
+			}
+		}
+	}
+}
