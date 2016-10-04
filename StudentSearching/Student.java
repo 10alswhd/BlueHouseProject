3d2fb1a5d4ec2ab490ed44064907e0f9d0468b3a
satin1004 <satin1004@naver.com>
2016-10-04 12:43:20 +0900

    Add files via upload

---
+++ StudentSearching/Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+	
+	String name;
+	String major;
+	String id;
+	double score;
+	
+	public Student(String name, String major, String id, double score){
+		this.name = name;
+		this.major = major;
+		this.id = id;
+		this.score = score;
+	}
+}
