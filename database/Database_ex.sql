c947cd679287bac4bf6822d36ef202e59bed4712
MJ <10alswhd@naver.com>
2016-10-25 01:55:36 +0900

    Create Database_ex.sql

---
+++ database/Database_ex.sql
@@ -0,0 +1,12 @@
+CREATE TABLE 'Usuarios' (
+  'Usuario' char(10) COLLATE latin1_general_ci NOT NULL,
+  'E' int(10) NOT NULL,
+  'N' int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+
+INSERT INTO 'Usuarios' VALUES('Alguien', 19, 3763);
+INSERT INTO 'Usuarios' VALUES('Roberto', 67, 253);
+INSERT INTO 'Usuarios' VALUES('Saul', 91459, 9131);
+INSERT INTO 'Usuarios' VALUES('Max', 233273, 73109);
+INSERT INTO 'Usuarios' VALUES('cecy', 59, 5767);
