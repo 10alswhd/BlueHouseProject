567dd374066b20c3152f76575ecc074130060e71
kukookoo <audrn6689@gmail.com>
2016-12-02 00:14:40 +0900

    Create log.c
    logb, logbf, logbl 함수 예제 코드, 밑수 2인 로그에서 정수

---
+++ etc/log.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+int main(void)
+{
+    printf("logb(1.0) %f\n", logb(1.0));
+    printf("log2(1.0) %f\n", log2(1.0));
+    printf("logb(2.0) %f\n", logb(2.0));
+    printf("log2(2.0) %f\n", log2(2.0));
+    printf("logb(3.0) %f\n", logb(3.0));
+    printf("log2(3.0) %f\n", log2(3.0));
+    printf("logb(4.0) %f\n", logb(4.0));
+    printf("log2(4.0) %f\n", log2(4.0));
+    printf("logb(5.0) %f\n", logb(5.0));
+    printf("log2(5.0) %f\n", log2(5.0));
+ 
+    return 0;
+}
