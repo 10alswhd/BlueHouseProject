65788aca15fd8ddba9c1ab58c9d33053fb2efd5f
jimin1992 <korea4647@naver.com>
2016-12-03 15:37:30 +0900

    Create keysearch.c

---
+++ search/keysearch.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+  char str[] = "This is a sample string";
+  char key[] = "aeiou";
+  char * pch;
+  printf ("Vowels in '%s': ",str);
+  pch = strpbrk (str, key);
+  while (pch != NULL)
+  {
+    printf ("%c " , *pch);
+    pch = strpbrk (pch+1,key);
+  }
+  printf ("\n");
+  return 0;
+} 
