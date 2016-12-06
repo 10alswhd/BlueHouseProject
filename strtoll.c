13f84fc3eb93b25ae22c6f76edd95b8db845b28e
kukookoo <audrn6689@gmail.com>
2016-12-06 21:26:07 +0900

    Create strtoll.c
    long long strtoll(const char * nptr,char **endptr,int base); 정수로 구성한 문자열로 long long 형식 값을 구함
    문자열의 정수를 원하는 진수 체계의 정수로 계산하여 출력

---
+++ strtoll.c
@@ -0,0 +1,19 @@
+ 
+#include <stdlib.h>
+#include <stdio.h>
+ 
+int main(void)
+{
+    char src[100] = "1234567890 123456789abcdef 16 100111111111111110000001100111110011110";
+    char *pos = NULL;
+    long long value1 = 0, value2 = 0, value3 = 0, value4 = 0;
+ 
+    value1 = strtoll(src, &pos, 10);//10진수로 계산
+    value2 = strtoll(pos, &pos, 16);//16진수로 계산
+    value3 = strtoll(pos, &pos, 8);//8진수로 계산
+    value4 = strtoll(pos, &pos, 2); //2진수로 계산
+    printf("%lld %lld %lld %lld\n", value1, value2, value3, value4);
+ 
+    return 0;
+}
+ 
