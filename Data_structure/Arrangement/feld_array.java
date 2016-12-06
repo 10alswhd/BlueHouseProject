6f32ddbc105484c9ae9537373f2f15cf9269fad2
MJ <10alswhd@naver.com>
2016-12-06 22:17:50 +0900

    Create feld_array.java

---
+++ Data_structure/Arrangement/feld_array.java
@@ -0,0 +1,14 @@
+Methode, wie man Strings vergleicht:
+
+String eins = "Bier";
+String zwei = "Wein";
+
+if (eins.equals(zwei)) {
+  System.out.println("Die Zeichenketten sind gleich.");
+else
+  System.out.println("Die Zeichenketten sind nicht gleich.");
+// auch möglich: if ("Wein".equals("Bier")) ...
+
+
+Zur Ausgabe (um das Ergebnis anzuzeigen)
+System.out.println("Die Getränke sind gleich.");
