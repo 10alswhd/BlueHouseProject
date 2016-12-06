41d7f8593bcf0ad51df1d94fd6a9f94c1fe18281
satin1004 <satin1004@naver.com>
2016-12-06 16:14:24 +0900

    Add files via upload

---
+++ etc/KeyBox-master/src/main/java/com/keybox/manage/model/User.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2013 Sean Kavanagh - sean.p.kavanagh6@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.keybox.manage.model;
+
+import java.util.List;
+
+/**
+ * Value object that contains user information
+ */
+public class User extends Auth {
+    String firstNm;
+    String lastNm;
+    String email;
+    boolean checked=false;
+    List<Profile> profileList;
+
+    public boolean getChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public String getFirstNm() {
+        return firstNm;
+    }
+
+    public void setFirstNm(String firstNm) {
+        this.firstNm = firstNm;
+    }
+
+    public String getLastNm() {
+        return lastNm;
+    }
+
+    public void setLastNm(String lastNm) {
+        this.lastNm = lastNm;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Profile> getProfileList() {
+        return profileList;
+    }
+
+    public void setProfileList(List<Profile> profileList) {
+        this.profileList = profileList;
+    }
+
+}
