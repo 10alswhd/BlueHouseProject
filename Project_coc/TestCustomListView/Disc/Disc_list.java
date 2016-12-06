ded03ab003e748512dab77755a3ef631396e2afc
MJ <10alswhd@naver.com>
2016-12-06 22:44:58 +0900

    Create Disc_list.java

---
+++ Project_coc/TestCustomListView/Disc/Disc_list.java
@@ -0,0 +1,36 @@
+package ex1.vo;
+
+import java.util.ArrayList;
+
+import android.R.integer;
+
+public class Disc_list {
+	ArrayList<Integer> listIntA = new ArrayList<Integer>();
+	ArrayList<Integer> listIntB = new ArrayList<Integer>();
+	
+	
+	public ArrayList<Integer> getListIntA1() {
+		return listIntA;
+	}
+	public void setListIntA1(Integer ii) {
+		
+		this.listIntA.add(ii);
+	}
+	
+	public ArrayList<Integer> getListIntB1() {
+		return listIntB;
+	}
+	public void setListIntB1(Integer ii) {
+		this.listIntB.add(ii);
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
