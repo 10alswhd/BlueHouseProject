022c8bbaef3218ce4297d320405da2f280f0b981
satin1004 <satin1004@naver.com>
2016-12-06 16:13:26 +0900

    Add files via upload

---
+++ etc/KeyBox-master/src/main/java/com/keybox/manage/model/PublicKey.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright 2013 Sean Kavanagh - sean.p.kavanagh6@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.keybox.manage.model;
+
+import java.util.Date;
+
+/**
+ * public key value object
+ */
+public class PublicKey {
+    Long id;
+    Long userId;
+    String username;
+    String keyNm;
+    String publicKey;
+    String type;
+    String fingerprint;
+    boolean enabled;
+    Date createDt;
+    Profile profile;
+    String passphrase;
+    String passphraseConfirm;
+
+
+    public String getKeyNm() {
+        return keyNm;
+    }
+
+    public void setKeyNm(String keyNm) {
+        this.keyNm = keyNm;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFingerprint() {
+        return fingerprint;
+    }
+
+    public void setFingerprint(String fingerprint) {
+        this.fingerprint = fingerprint;
+    }
+
+    public Date getCreateDt() {
+        return createDt;
+    }
+
+    public void setCreateDt(Date createDt) {
+        this.createDt = createDt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getPassphraseConfirm() {
+        return passphraseConfirm;
+    }
+
+    public void setPassphraseConfirm(String passphraseConfirm) {
+        this.passphraseConfirm = passphraseConfirm;
+    }
+
+    public String getPassphrase() {
+        return passphrase;
+    }
+
+    public void setPassphrase(String passphrase) {
+        this.passphrase = passphrase;
+    }
+}
