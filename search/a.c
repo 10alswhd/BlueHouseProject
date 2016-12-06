12702a8f7d7a33c1bfccda21d41d80cadfcae3d5
jimin1992 <korea4647@naver.com>
2016-12-06 21:39:37 +0900

    Create a.c

---
+++ search/a.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+    //https://www.google.com.my/search?q=6%2F2(1%2B2)&sugexp=chrome,mod=7&sourceid=chrome&ie=UTF-8
+    a = 6/2 * (1+2);
+    return printf("a=%d\r\n" ,a);
+}
