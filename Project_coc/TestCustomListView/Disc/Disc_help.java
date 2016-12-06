c1130daf5450b5f1686807799fd9adeda1637cd1
MJ <10alswhd@naver.com>
2016-12-06 22:41:41 +0900

    Create Disc_help.java

---
+++ Project_coc/TestCustomListView/Disc/Disc_help.java
@@ -0,0 +1,15 @@
+package ex1.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Disc_help  extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.help);
+	}
+	
+}
