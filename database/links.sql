bc8f8e8e1034288cb25280f1b4fd60bcb405de46
simvery <sim960425@naver.com>
2016-12-07 00:46:20 +0900

    Create links.sql

---
+++ database/links.sql
@@ -0,0 +1,12 @@
+--
+--  All DB links defined in the database
+--
+select * from DBA_DB_LINKS;
+--
+-- All DB links the curernt user has access to
+--
+select * from ALL_DB_LINKS;
+--
+-- All DB links owned by current user
+--
+select * from USER_DB_LINKS;
