c23db9e1f8f5c8a5ef0f71b28d6f04a7ae021173
MJ <10alswhd@naver.com>
2016-12-06 22:43:23 +0900

    Create Disc_EndCart.java

---
+++ Project_coc/TestCustomListView/Disc/Disc_EndCart.java
@@ -0,0 +1,1607 @@
+package ex1.vo;
+
+
+public class Disc_EndCart {
+	int a1,a2,a3, a4, b1,b2, b3, b4;
+	int ca1,ca2,ca3,ca4,ca5,cb1,cb2,cb3,cb4,cb5;
+	
+	int ex1,ex2,ex3,ex4;
+
+	public Disc_EndCart() {
+		super();
+	}
+
+	public void Disc_EndCart3(int a1, int a2, int a3, int a4, int b1, int b2,int b3, int b4){
+		this.a1 = a1;
+		this.a2 = a2;
+		this.a3 = a3;
+		this.a4 = a4;
+		this.b1 = b1;
+		this.b2 = b2;
+		this.b3 = b3;
+		this.b4 = b4;
+		
+		ex1char();
+		ex2char();
+		ex3char();
+		ex4char();
+		
+		
+		
+		
+		
+		
+	}
+
+	private void ex4char() {
+		switch (a4 - b4) {
+		case -28:
+			ex4 = 2;
+			break;
+		case -27:
+			ex4 = 2;
+			break;
+		case -26:
+			ex4 = 3;
+			break;
+		case -25:
+			ex4 = 3;
+			break;
+		case -24:
+			ex4 = 4;
+			break;
+		case -23:
+			ex4 = 4;
+			break;
+		case -22:
+			ex4 = 5;
+			break;
+		case -21:
+			ex4 = 5;
+			break;
+		case -20:
+			ex4 = 6;
+			break;
+		case -19:
+			ex4 = 6;
+			break;
+		case -18:
+			ex4 = 7;
+			break;
+		case -17:
+			ex4 = 7;
+			break;
+		case -16:
+			ex4 = 8;
+			break;
+		case -15:
+			ex4 = 9;
+			break;
+		case -14:
+			ex4 = 10;
+			break;
+		case -13:
+			ex4 = 11;
+			break;
+		case -12:
+			ex4 = 12;
+			break;
+		case -11:
+			ex4 = 14;
+			break;
+		case -10:
+			ex4 = 21;
+			break;
+		case -9:
+			ex4 = 35;
+			break;
+		case -8:
+			ex4 = 56;
+			break;
+		case -7:
+			ex4 = 63;
+			break;
+		case -6:
+			ex4 = 70;
+			break;
+		case -5:
+			ex4 = 77;
+			break;
+		case -4:
+			ex4 = 98;
+			break;
+		case -3:
+			ex4 = 105;
+			break;
+		case -2:
+			ex4 = 126;
+			break;
+		case -1:
+			ex4 = 133;
+			break;
+		case 0:
+			ex4 = 140;
+			break;
+		case 1:
+			ex4 = 147;
+			break;
+		case 2:
+			ex4 = 168;
+			break;
+		case 3:
+			ex4 = 175;
+			break;
+		case 4:
+			ex4 = 189;
+			break;
+		case 5:
+			ex4 = 196;
+			break;
+		case 6:
+			ex4 = 210;
+			break;
+		case 7:
+			ex4 = 211;
+			break;
+		case 8:
+			ex4 = 212; 
+			break;
+		case 9:
+			ex4 =  213;
+			break;
+		case 10:
+			ex4 =  214;
+			break;
+		case 11:
+			ex4 =  215;
+			break;
+		case 12:
+			ex4 =  216;
+			break;
+		case 13:
+			ex4 =  217;
+			break;
+		case 14:
+			ex4 =  218;
+			break;
+		case 15:
+			ex4 =  219;
+			break;
+		case 16:
+			ex4 =  220;
+			break;
+		case 17:
+			ex4 =  221;
+			break;
+		case 18:
+			ex4 = 231; 
+			break;
+		case 19:
+			ex4 =  233;
+			break;
+		case 20:
+			ex4 =  234;
+			break;
+		case 21:
+			ex4 =  235;
+			break;
+		case 22:
+			ex4 = 236; 
+			break;
+		case 23:
+			ex4 = 237;
+			break;
+		case 24:
+			ex4 = 238;
+			break;
+		case 25:
+			ex4 =  239;
+			break;
+		case 26:
+			ex4 =  240;
+			break;
+		case 27:
+			ex4 =   241; 
+			break;
+		case 28:
+			ex4 =   242;
+			break;
+		
+		}
+		
+	}
+
+	private void ex3char() {
+		switch (a3 - b3) {
+		case -28:
+			ex3 = 6;
+			break;
+		case -27:
+			ex3 = 7;
+			break;
+		case -26:
+			ex3 = 9;
+			break;
+		case -25:
+			ex3 = 10;
+			break;
+		case -24:
+			ex3 = 11;
+			break;
+		case -23:
+			ex3 = 12;
+			break;
+		case -22:
+			ex3 = 13;
+			break;
+		case -21:
+			ex3 = 14;
+			break;
+		case -20:
+			ex3 = 15;
+			break;
+		case -19:
+			ex3 = 16;
+			break;
+		case -18:
+			ex3 = 17;
+			break;
+		case -17:
+			ex3 = 18;
+			break;
+		case -16:
+			ex3 = 19;
+			break;
+		case -15:
+			ex3 = 20;
+			break;
+		case -14:
+			ex3 = 21;
+			break;
+		case -13:
+			ex3 = 22;
+			break;
+		case -12:
+			ex3 = 23;
+			break;
+		case -11:
+			ex3 = 24;
+			break;
+		case -10:
+			ex3 = 25;
+			break;
+		case -9:
+			ex3 = 26;
+			break;
+		case -8:
+			ex3 = 27;
+			break;
+		case -7:
+			ex3 = 28;
+			break;
+		case -6:
+			ex3 = 31;
+			break;
+		case -5:
+			ex3 = 35;
+			break;
+		case -4:
+			ex3 = 42;
+			break;
+		case -3:
+			ex3 = 53;
+			break;
+		case -2:
+			ex3 = 60;
+			break;
+		case -1:
+			ex3 = 65;
+			break;
+		case 0:
+			ex3 = 77;
+			break;
+		case 1:
+			ex3 = 98;
+			break;
+		case 2:
+			ex3 = 105;
+			break;
+		case 3:
+			ex3 = 112;
+			break;
+		case 4:
+			ex3 = 119;
+			break;
+		case 5:
+			ex3 = 126;
+			break;
+		case 6:
+			ex3 = 140;
+			break;
+		case 7:
+			ex3 = 161;
+			break;
+		case 8:
+			ex3 = 168;
+			break;
+		case 9:
+			ex3 = 182;
+			break;
+		case 10:
+			ex3 = 196;
+			break;
+		case 11:
+			ex3 = 203;
+			break;
+		case 12:
+			ex3 = 210;
+			break;
+		case 13:
+			ex3 = 217;
+			break;
+		case 14:
+			ex3 = 224;
+			break;
+		case 15:
+			ex3 = 225;
+			break;
+		case 16:
+			ex3 = 226;
+			break;
+		case 17:
+			ex3 = 227;
+			break;
+		case 18:
+			ex3 = 228;
+			break;
+		case 19:
+			ex3 = 229;
+			break;
+		case 20:
+			ex3 = 230;
+			break;
+		case 21:
+			ex3 = 231;
+			break;
+		case 22:
+			ex3 = 232;
+			break;
+		case 23:
+			ex3 = 233;
+			break;
+		case 24:
+			ex3 = 234;
+			break;
+		case 25:
+			ex3 = 236;
+			break;
+		case 26:
+			ex3 = 238;
+			break;
+		case 27:
+			ex3 = 239;
+			break;
+		case 28:
+			ex3 = 240;
+			break;
+		
+		}
+	}
+
+	private void ex2char() {
+		switch (a2 - b2) {
+		case -28:
+			ex2 = 5;
+			break;
+		case -27:
+			ex2 = 6;
+			break;
+		case -26:
+			ex2 = 7;
+			break;
+		case -25:
+			ex2 = 8;
+			break;
+		case -24:
+			ex2 =9; 
+			break;
+		case -23:
+			ex2 = 10;
+			break;
+		case -22:
+			ex2 = 11;
+			break;
+		case -21:
+			ex2 = 12;
+			break;
+		case -20:
+			ex2 = 13;
+			break;
+		case -19:
+			ex2 = 14;
+			break;
+		case -18:
+			ex2 = 15;
+			break;
+		case -17:
+			ex2 = 16;
+			break;
+		case -16:
+			ex2 = 17;
+			break;
+		case -15:
+			ex2 = 18;
+			break;
+		case -14:
+			ex2 = 19;
+			break;
+		case -13:
+			ex2 = 20;
+			break;
+		case -12:
+			ex2 = 21;
+			break;
+		case -11:
+			ex2 = 22;
+			break;
+		case -10:
+			ex2 = 23;
+			break;
+		case -9:
+			ex2 = 24;
+			break;
+		case -8:
+			ex2 = 28;
+			break;
+		case -7:
+			ex2 = 31;
+			break;
+		case -6:
+			ex2 = 35;
+			break;
+		case -5:
+			ex2 = 42;
+			break;
+		case -4:
+			ex2 = 49;
+			break;
+		case -3:
+			ex2 = 63;
+			break;
+		case -2:
+			ex2 = 77;
+			break;
+		case -1:
+			ex2 = 98;
+			break;
+		case 0:
+			ex2 = 105;
+			break;
+		case 1:
+			ex2 = 119;
+			break;
+		case 2:
+			ex2 = 126;
+			break;
+		case 3:
+			ex2 = 147;
+			break;
+		case 4:
+			ex2 = 161;
+			break;
+		case 5:
+			ex2 = 168;
+			break;
+		case 6:
+			ex2 = 182;
+			break;
+		case 7:
+			ex2 = 196;
+			break;
+		case 8:
+			ex2 = 206;
+			break;
+		case 9:
+			ex2 = 217;
+			break;
+		case 10:
+			ex2 = 218;
+			break;
+		case 11:
+			ex2 = 219;
+			break;
+		case 12:
+			ex2 = 220;
+			break;
+		case 13:
+			ex2 = 221; 
+			break;
+		case 14:
+			ex2 =  222;
+			break;
+		case 15:
+			ex2 =  223;
+			break;
+		case 16:
+			ex2 =  224;
+			break;
+		case 17:
+			ex2 =  225;
+			break;
+		case 18:
+			ex2 =  226;
+			break;
+		case 19:
+			ex2 =  227;
+			break;
+		case 20:
+			ex2 =  228;
+			break;
+		case 21:
+			ex2 =  231;
+			break;
+		case 22:
+			ex2 =  232;
+			break;
+		case 23:
+			ex2 =  233;
+			break;
+		case 24:
+			ex2 =  234;
+			break;
+		case 25:
+			ex2 =  235;
+			break;
+		case 26:
+			ex2 =  236;
+			break;
+		case 27:
+			ex2 = 237; 
+			break;
+		case 28:
+			ex2 = 238;
+			break;
+		
+		}
+		
+	}
+
+	private void ex1char() {
+		switch (a1 - b1) {
+		case -28:
+			ex1 = 0;
+			break;
+		case -27:
+			ex1 = 7;
+			break;
+		case -26:
+			ex1 = 8;
+			break;
+		case -25:
+			ex1 = 9;
+			break;
+		case -24:
+			ex1 = 10;
+			break;
+		case -23:
+			ex1 = 11;
+			break;
+		case -22:
+			ex1 = 12;
+			break;
+		case -21:
+			ex1 = 13;
+			break;
+		case -20:
+			ex1 = 14;
+			break;
+		case -19:
+			ex1 = 15;
+			break;
+		case -18:
+			ex1 = 16;
+			break;
+		case -17:
+			ex1 = 17;
+			break;
+		case -16:
+			ex1 = 28;
+			break;
+		case -15:
+			ex1 = 35;
+			break;
+		case -14:
+			ex1 = 42;
+			break;
+		case -13:
+			ex1 = 56;
+			break;
+		case -12:
+			ex1 = 70;
+			break;
+		case -11:
+			ex1 = 77;
+			break;
+		case -10:
+			ex1 = 91;
+			break;
+		case -9:
+			ex1 = 98;
+			break;
+		case -8:
+			ex1 = 105;
+			break;
+		case -7:
+			ex1 = 112;
+			break;
+		case -6:
+			ex1 = 119;
+			break;
+		case -5:
+			ex1 = 133;
+			break;
+		case -4:
+			ex1 = 147;
+			break;
+		case -3:
+			ex1 = 154;
+			break;
+		case -2:
+			ex1 = 161;
+			break;
+		case -1:
+			ex1 = 168;
+			break;
+		case 0:
+			ex1 = 171;
+			break;
+		case 1:
+			ex1 = 175;
+			break;
+		case 2:
+			ex1 = 182;
+			break;
+		case 3:
+			ex1 = 189;
+			break;
+		case 4:
+			ex1 = 196;
+			break;
+		case 5:
+			ex1 = 203;
+			break;
+		case 6:
+			ex1 = 208;
+			break;
+		case 7:
+			ex1 = 213;
+			break;
+		case 8:
+			ex1 = 218;
+			break;
+		case 9:
+			ex1 = 224;
+			break;
+		case 10:
+			ex1 = 223;
+			break;
+		case 11:
+			ex1 = 224;
+			break;
+		case 12:
+			ex1 = 225;
+			break;
+		case 13:
+			ex1 = 226;
+			break;
+		case 14:
+			ex1 = 227;
+			break;
+		case 15:
+			ex1 = 228;
+			break;
+		case 16:
+			ex1 = 229;
+			break;
+		case 17:
+			ex1 = 230;
+			break;
+		case 18:
+			ex1 = 231;
+			break;
+		case 19:
+			ex1 = 232;
+			break;
+		case 20:
+			ex1 = 233;
+			break;
+		case 21:
+			ex1 = 234;
+			break;
+		case 22:
+			ex1 = 235;
+			break;
+		case 23:
+			ex1 = 236;
+			break;
+		case 24:
+			ex1 = 237;
+			break;
+		case 25:
+			ex1 = 238;
+			break;
+		case 26:
+			ex1 = 238;
+			break;
+		case 27:
+			ex1 = 238;
+			break;
+		case 28:
+			ex1 = 239;
+			break;
+		
+		}
+		
+	}
+
+	public void Disc_EndCart1(int a1, int a2, int a3, int a4) {
+
+		this.a1 = a1;
+		this.a2 = a2;
+		this.a3 = a3;
+		this.a4 = a4;
+	
+		a1char();
+		a2char();
+		a3char();
+		a4char();
+		
+	
+		
+		
+		
+		
+		
+	}
+	
+	public void Disc_EndCart2(int b1, int b2,int b3, int b4) {
+		this.b1 = b1;
+		this.b2 = b2;
+		this.b3 = b3;
+		this.b4 = b4;
+		
+		
+		b1char();
+		b2char();
+		b3char();
+		b4char();
+		
+		
+	}
+	
+	
+	
+	private void b4char() {
+		switch (b4) {
+		case 0:
+			cb4 = 7;
+			break;
+		case 1:
+			cb4 = 14;
+			break;
+		case 2:
+			cb4 = 21;
+			break;
+		case 3:
+			cb4 = 28;
+			break;
+		case 4:
+			cb4 = 42;
+			break;
+		case 5:
+			cb4 = 70;
+			break;
+		case 6:
+			cb4 = 91;
+			break;
+		case 7:
+			cb4 = 119;
+			break;
+		case 8:
+			cb4 = 140;
+			break;
+		case 9:
+			cb4 = 154;
+			break;
+		case 10:
+			cb4 = 182;
+			break;
+		case 11:
+			cb4 = 196;
+			break;
+		case 12:
+			cb4 = 210;
+			break;
+		case 13:
+			cb4 = 217;
+			break;
+		case 14:
+			cb4 = 220;
+			break;
+		case 15:
+			cb4 = 222;
+			break;
+		case 16:
+			cb4 = 224;
+			break;
+		case 17:
+			cb4 = 226;
+			break;
+		case 18:
+			cb4 = 228;
+			break;
+		case 19:
+			cb4 = 230;
+			break;
+		case 20:
+			cb4 = 232;
+			break;
+		case 21:
+			cb4 = 234;
+			break;
+		case 22:
+			cb4 = 236;
+			break;
+		case 23:
+			cb4 = 238;
+			break;
+		case 24:
+			cb4 = 240;
+			break;
+		case 25:
+			cb4 = 242;
+			break;
+		case 26:
+			cb4 = 244;
+			break;
+		case 27:
+			cb4 = 246;
+			break;
+		case 28:
+			cb4 = 248;
+			break;
+
+		
+		}
+		
+	}
+
+	private void b3char() {
+		switch (b3) {
+		case 0:
+			cb3 = 7;
+			break;
+		case 1:
+			cb3 = 21;
+			break;
+		case 2:
+			cb3 = 63;
+			break;
+		case 3:
+			cb3 = 98;
+			break;
+		case 4:
+			cb3 = 119;
+			break;
+		case 5:
+			cb3 = 147;
+			break;
+		case 6:
+			cb3 = 168;
+			break;
+		case 7:
+			cb3 = 189;
+			break;
+		case 8:
+			cb3 = 196;
+			break;
+		case 9:
+			cb3 = 210;
+			break;
+		case 10:
+			cb3 = 217;
+			break;
+		case 11:
+			cb3 = 239;
+			break;
+		case 12:
+			cb3 = 240;
+			break;
+		case 13:
+			cb3 = 240;
+			break;
+		case 14:
+			cb3 = 241;
+			break;
+		case 15:
+			cb3 = 241;
+			break;
+		case 16:
+			cb3 = 242;
+			break;
+		case 17:
+			cb3 = 242;
+			break;
+		case 18:
+			cb3 = 243;
+			break;
+		case 19:
+			cb3 = 243;
+			break;
+		case 20:
+			cb3 = 244;
+			break;
+		case 21:
+			cb3 = 244;
+			break;
+		case 22:
+			
+			cb3 = 245;
+			break;
+		case 23:
+			cb3 = 245;
+			break;
+		case 24:
+			cb3 = 246;
+			break;
+		case 25:
+			cb3 = 246;
+			break;
+		case 26:
+			cb3 = 247;
+			break;
+		case 27:
+			cb3 = 247;
+			break;
+		case 28:
+			cb3 = 248;
+			break;
+
+		
+		}
+		
+	}
+
+	private void b2char() {
+		switch (b2) {
+		case 0:
+			cb2 = 7;
+			break;
+		case 1:
+			cb2 = 21;
+			break;
+		case 2:
+			cb2 = 35;
+			break;
+		case 3:
+			cb2 = 42;
+			break;
+		case 4:
+			cb2 = 84;
+			break;
+		case 5:
+			cb2 = 119;
+			break;
+		case 6:
+			cb2 = 147;
+			break;
+		case 7:
+			cb2 = 168;
+			break;
+		case 8:
+			cb2 = 189;
+			break;
+		case 9:
+			cb2 = 210;
+			break;
+		case 10:
+			cb2 = 217;
+			break;
+		case 11:
+			cb2 = 220;
+			break;
+		case 12:
+			cb2 = 222;
+			break;
+		case 13:
+			cb2 = 224;
+			break;
+		case 14:
+			cb2 = 226;
+			break;
+		case 15:
+			cb2 = 228;
+			break;
+		case 16:
+			cb2 = 230;
+			break;
+		case 17:
+			cb2 = 232;
+			break;
+		case 18:
+			cb2 = 234;
+			break;
+		case 19:
+			cb2 = 236;
+			break;
+		case 20:
+			cb2 = 238;
+			break;
+		case 21:
+			cb2 = 240;
+			break;
+		case 22:
+			cb2 = 242;
+			break;
+		case 23:
+			cb2 = 243;
+			break;
+		case 24:
+			cb2 = 244;
+			break;
+		case 25:
+			cb2 = 245;
+			break;
+		case 26:
+			cb2 = 246;
+			break;
+		case 27:
+			cb2 = 247;
+			break;
+		case 28:
+			cb2 = 248;
+			break;
+
+		
+		}
+		
+	}
+
+	private void b1char() {
+		switch (b1) {
+		case 0:
+			cb1 = 7;
+			break;
+		case 1:
+			cb1 = 14;
+			break;
+		case 2:
+			cb1 = 21;
+			break;
+		case 3:
+			cb1 = 28;
+			break;
+		case 4:
+			cb1 = 35;
+			break;
+		case 5:
+			cb1 = 42;
+			break;
+		case 6:
+			cb1 = 63;
+			break;
+		case 7:
+			cb1 = 70;
+			break;
+		case 8:
+			cb1 = 84;
+			break;
+		case 9:
+			cb1 = 112;
+			break;
+		case 10:
+			cb1 = 133;
+			break;
+		case 11:
+			cb1 = 147;
+			break;
+		case 12:
+			cb1 = 154;
+			break;
+		case 13:
+			cb1 = 168;
+			break;
+		case 14:
+			cb1 = 189;
+			break;
+		case 15:
+			cb1 = 203;
+			break;
+		case 16:
+			cb1 = 210;
+			break;
+		case 17:
+			cb1 = 217;
+			break;
+		case 18:
+			cb1 = 220;
+			break;
+		case 19:
+			cb1 = 222;
+			break;
+		case 20:
+			cb1 = 224;
+			break;
+		case 21:
+			cb1 = 226;
+			break;
+		case 22:
+			cb1 = 228;
+			break;
+		case 23:
+			cb1 = 230;
+			break;
+		case 24:
+			cb1 = 232;
+			break;
+		case 25:
+			cb1 = 234;
+			break;
+		case 26:
+			cb1 = 236;
+			break;
+		case 27:
+			cb1 = 238;
+			break;
+		case 28:
+			cb1 = 248;
+			break;
+
+		
+		}
+		
+	}
+
+
+	private void a4char() {
+		switch (a4) {
+		case 0:
+			ca4 = 7;
+			break;
+		case 1:
+			ca4 = 14;
+			break;
+		case 2:
+			ca4 = 28;
+			break;
+		case 3:
+			ca4 = 56;
+			break;
+		case 4:
+			ca4 = 77;
+			break;
+		case 5:
+			ca4 = 105;
+			break;
+		case 6:
+			ca4 = 120;
+			break;
+		case 7:
+			ca4 = 144;
+			break;
+		case 8:
+			ca4 = 168;
+			break;
+		case 9:
+			ca4 = 189;
+			break;
+		case 10:
+			ca4 = 196;
+			break;
+		case 11:
+			ca4 = 210;
+			break;
+		case 12:
+			ca4 = 217;
+			break;
+		case 13:
+			ca4 = 218;
+			break;
+		case 14:
+			ca4 = 220;
+			break;
+		case 15:
+			ca4 = 222;
+			break;
+		case 16:
+			ca4 = 224;
+			break;
+		case 17:
+			ca4 = 226;
+			break;
+		case 18:
+			ca4 = 228;
+			break;
+		case 19:
+			ca4 = 230;
+			break;
+		case 20:
+			ca4 = 232;
+			break;
+		case 21:
+			ca4 = 234;
+			break;
+		case 22:
+			ca4 = 236;
+			break;
+		case 23:
+			ca4 = 238;
+			break;
+		case 24:
+			ca4 = 240;
+			break;
+		case 25:
+			ca4 = 242;
+			break;
+		case 26:
+			ca4 = 244;
+			break;
+		case 27:
+			ca4 = 246;
+			break;
+		case 28:
+			ca4 = 248;
+			break;
+
+		
+		}
+		
+	}
+
+	private void a3char() {
+		switch (a3) {
+		case 0:
+			ca3 = 7;
+			break;
+		case 1:
+			ca3 = 21;
+			break;
+		case 2:
+			ca3 = 28;
+			break;
+		case 3:
+			ca3 = 35;
+			break;
+		case 4:
+			ca3 = 56;
+			break;
+		case 5:
+			ca3 = 77;
+			break;
+		case 6:
+			ca3 = 91;
+			break;
+		case 7:
+			ca3 = 105;
+			break;
+		case 8:
+			ca3 = 119;
+			break;
+		case 9:
+			ca3 = 133;
+			break;
+		case 10:
+			ca3 = 144;
+			break;
+		case 11:
+			ca3 = 168;
+			break;
+		case 12:
+			ca3 = 189;
+			break;
+		case 13:
+			ca3 = 203;
+			break;
+		case 14:
+			ca3 = 210;
+			break;
+		case 15:
+			ca3 = 212;
+			break;
+		case 16:
+			ca3 = 214;
+			break;
+		case 17:
+			ca3 = 216;
+			break;
+		case 18:
+			ca3 = 218;
+			break;
+		case 19:
+			ca3 = 220;
+			break;
+		case 20:
+			ca3 = 222;
+			break;
+		case 21:
+			ca3 = 224;
+			break;
+		case 22:
+			ca3 = 226;
+			break;
+		case 23:
+			ca3 = 228;
+			break;
+		case 24:
+			ca3 = 230;
+			break;
+		case 25:
+			ca3 = 232;
+			break;
+		case 26:
+			ca3 = 234;
+			break;
+		case 27:
+			ca3 = 236;
+			break;
+		case 28:
+			ca3 = 242;
+			break;
+
+		
+		}
+		
+	}
+
+	private void a2char() {
+		switch (a2) {
+		case 0:
+			ca2 = 7;
+			break;
+		case 1:
+			ca2 = 14;
+			break;
+		case 2:
+			ca2 = 28;
+			break;
+		case 3:
+			ca2 = 49;
+			break;
+		case 4:
+			ca2 = 77;
+			break;
+		case 5:
+			ca2 = 98;
+			break;
+		case 6:
+			ca2 = 119;
+			break;
+		case 7:
+			ca2 = 126;
+			break;
+		case 8:
+			ca2 = 144;
+			break;
+		case 9:
+			ca2 = 175;
+			break;
+		case 10:
+			ca2 = 189;
+			break;
+		case 11:
+			ca2 = 203;
+			break;
+		case 12:
+			ca2 = 210;
+			break;
+		case 13:
+			ca2= 212;
+			break;
+		case 14:
+			ca2= 214;
+			break;
+		case 15:
+			ca2= 216;
+			break;
+		case 16:
+			ca2= 218;
+			break;
+		case 17:
+			ca2= 220;
+			break;
+		case 18:
+			ca2= 222;
+			break;
+		case 19:
+			ca2= 224;
+			break;
+		case 20:
+			ca2= 226;
+			break;
+		case 21:
+			ca2= 228;
+			break;
+		case 22:
+			ca2= 230;
+			break;
+		case 23:
+			ca2= 232;
+			break;
+		case 24:
+			ca2= 234;
+			break;
+		case 25:
+			ca2= 236;
+			break;
+		case 26:
+			ca2= 238;
+			break;
+		case 27:
+			ca2= 240;
+			break;
+		case 28:
+			ca2= 242;
+			break;
+
+
+		
+		}		
+	}
+
+	private void a1char() {
+		switch (a1) {
+		case 0:
+			ca1= 21;
+			break;
+		case 1:
+			ca1= 49;
+			break;
+		case 2:
+			ca1= 77;
+			break;
+		case 3:
+			ca1= 98;
+			break;
+		case 4:
+			ca1= 112;
+			break;
+		case 5:
+			ca1= 126;
+			break;
+		case 6:
+			ca1= 140;
+			break;
+		case 7:
+			ca1= 147;
+			break;
+		case 8:
+			ca1= 175;
+			break;
+		case 9:
+			ca1= 189;
+			break;
+		case 10:
+			ca1= 196;
+			break;
+		case 11:
+			ca1= 203;
+			break;
+		case 12:
+			ca1= 210;
+			break;
+		case 13:
+			ca1= 212;
+			break;
+		case 14:
+			ca1= 214;
+			break;
+		case 15:
+			ca1= 216;
+			break;
+		case 16:
+			ca1= 218;
+			break;
+		case 17:
+			ca1= 220;
+			break;
+		case 18:
+			ca1= 222;
+			break;
+		case 19:
+			ca1= 224;
+			break;
+		case 20:
+			ca1= 226;
+			break;
+		case 21:
+			ca1= 228;
+			break;
+		case 22:
+			ca1= 230;
+			break;
+		case 23:
+			ca1= 232;
+			break;
+		case 24:
+			ca1= 234;
+			break;
+		case 25:
+			ca1= 236;
+			break;
+		case 26:
+			ca1= 238;
+			break;
+		case 27:
+			ca1= 240;
+			break;
+		case 28:
+			ca1= 242;
+			break;
+
+		
+		}
+		
+	}
+
+
+	public int getCa1() {
+		return ca1;
+	}
+
+
+	public int getCa2() {
+		return ca2;
+	}
+
+
+	public int getCa3() {
+		return ca3;
+	}
+
+
+	public int getCa4() {
+		return ca4;
+	}
+
+
+	public int getCb1() {
+		return cb1;
+	}
+
+
+	public int getCb2() {
+		return cb2;
+	}
+
+
+	public int getCb3() {
+		return cb3;
+	}
+
+
+	public int getCb4() {
+		return cb4;
+	}
+
+	public int getEx1() {
+		return ex1;
+	}
+
+	public int getEx2() {
+		return ex2;
+	}
+
+	public int getEx3() {
+		return ex3;
+	}
+
+	public int getEx4() {
+		return ex4;
+	}
+
+	
+	
+	
+	
+	
+}
