5bd83cb484b6e63378d50d524dd9f9041214dd64
jimin1992 <korea4647@naver.com>
2016-12-06 23:43:41 +0900

    Create bla.c

---
+++ search/bla.c
@@ -0,0 +1,16 @@
+/* confdefs.h.  */
+
+#define PACKAGE_NAME ""
+#define PACKAGE_TARNAME ""
+#define PACKAGE_VERSION ""
+#define PACKAGE_STRING ""
+#define PACKAGE_BUGREPORT ""
+/* end confdefs.h.  */
+
+int
+main ()
+{
+
+  ;
+  return 0;
+}
