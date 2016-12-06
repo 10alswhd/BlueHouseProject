9cffbcb5e4fa7a9e656f2c078af65b1a07b2ad7a
MJ <10alswhd@naver.com>
2016-12-06 22:07:15 +0900

    Create Android Array Resource.java

---
+++ Data_structure/Arrangement/Android Array Resource.java
@@ -0,0 +1,19 @@
+<resources>
+
+    <string-array name="student_names">
+        <item>"neil"</item>
+        <item>"srini"</item>
+        <item>"matt"</item>
+    </string-array>
+
+    <string-array name="student_roles">
+        <item>"@string/teacher"</item>
+        <item>"student"</item>
+        <item>"trouble_maker"</item>
+    </string-array>
+
+</resources>
+
+
+//CODE
+//String[] students = getResources().getStringArray(R.array.student_names);
