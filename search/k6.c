253774ecff5f957f55aecb3f6eb92546a3d33f33
jimin1992 <korea4647@naver.com>
2016-12-13 23:46:23 +0900

    Create k6.c

---
+++ search/k6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+  int i,r;
+  i = rand();
+  i = i%2;
+  int num = 50; 
+  int x[num];
+  int b[10] = {0};
+  srand((unsigned int) time(NULL));
+
+  for(i=0; i<num; i++) {
+    r = rand() % 10; 
+    x[i] = r;
+    b[r]++;
+    printf(" %d,",r);
+    if(i%10 == 9) {
+      printf("\n");
+    }   
+  }
+
+  for(i=0; i<10; i++)
+    printf("%dは%d個\n",i,b[i]);
+
+  return 0;
+}
