07ce5a7f6e2c5382bf32806c338e8e1a4f2afc77
jimin1992 <korea4647@naver.com>
2016-12-13 22:57:54 +0900

    Create Bplustree.c

---
+++ search/Bplustree.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bintree.h"
+#include "Bplustree.h"
+
+
+
+
+int main(void)
+{
+    return 0;
+}
+
+
+record_t *newREC(char *rec_val)
+{
+}
+
+
+Bplustree_t *newBPtree(void)
+{
+}
+
+
+Bplustree_t BPinsert(Bplustree_t *tree, record_t *r)
+{
+}
+
+
+char *BPselect(Bplustree_t *tree, int key)
+{
+}
+
+
+void BPdelete(Bplustree_t *tree, int key)
+{
+}
