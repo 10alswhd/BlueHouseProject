444461b31c7637b91f8154775b867ad4798e4bef
jimin1992 <korea4647@naver.com>
2016-12-13 23:49:20 +0900

    Create brilliant.h

---
+++ search/brilliant.h
@@ -0,0 +1,21 @@
+#ifndef BRILLIANT_H
+#define BRILLIANT_H
+
+//greatest common divisor
+int gcd(int a, int b);
+int gcd3(int a, int b, int c);
+
+
+//reverse integer
+int reverse(int n);
+
+//is prime
+int isPrime(int n);
+
+int min(int a, int b);
+int max(int a, int b);
+
+//qsort comparison function for integers
+int cmpInt(const void *a, const void *b);
+
+#endif
