14be09449d9309bd83aed09fbdad0c3598c2d497
MJ <10alswhd@naver.com>
2016-12-13 18:22:46 +0900

    Create Animal.swift

---
+++ swift/Animal.swift
@@ -0,0 +1,11 @@
+class Animal {
+  var name: String
+  var age: Int
+  var image: UIImage
+
+  init(name: String, age: Int, image: UIImage) {
+    self.name = name
+    self.age = age
+    self.image = image
+  }
+}
