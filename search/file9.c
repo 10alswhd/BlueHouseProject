18d375f3a9d4492cab018d97a0597a0491f25a79
jimin1992 <korea4647@naver.com>
2016-12-13 22:55:54 +0900

    Create file9.c

---
+++ search/file9.c
@@ -0,0 +1,11 @@
+<前略>
+  :
+    const int debug = 0;
+
+    if (debug) {
+    printf("size: %"PRIdSIZE"\n", size);
+    printf("table[%"PRIdSIZE"]: position: %d, line: %d, pos: %"PRIdSIZE"\n",
+           i, table[i].position, table[i].line_no, pos);
+    }
+  :
+<後略>
