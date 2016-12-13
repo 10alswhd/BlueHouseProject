d5db11d12f8feed736e701503c94fd9fe67bc37c
jimin1992 <korea4647@naver.com>
2016-12-13 23:45:43 +0900

    Create k1.c

---
+++ search/k1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int i,j;
+  int param;
+  
+  param = atoi(argv[1]);
+
+  for(i=0; i<param; i++) {
+    for(j=0; j<i+1; j++) {
+      printf("x");
+    }   
+    printf("\n");
+  }
+
+  return 0;
+}
