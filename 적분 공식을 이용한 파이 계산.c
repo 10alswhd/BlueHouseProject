5fa0ae8602e681a1ee4656f5ce9a52cf0e62e4c1
kukookoo <audrn6689@gmail.com>
2016-12-13 17:25:45 +0900

    Create 적분 공식을 이용한 파이 계산.c

---
+++ 적분 공식을 이용한 파이 계산.c
@@ -0,0 +1,25 @@
+
+//적분 공식을 이용한 파이 구하기
+#include <stdio.h>
+ 
+int main(void)
+{
+    double pi = 0;
+    int k = 0;
+ 
+    while (k<10000000) //반복문의 횟수가 많을 수록 PI값과 비슷합니다.
+    {
+        ++k;
+        if (k % 2)
+        {
+            pi += 4.0 / (2.0 * k - 1.0);
+        }
+        else
+        {
+            pi -= 4.0 / (2.0 * k - 1.0);
+        }
+    }
+ 
+    printf("pi = %.15f\n", pi);
+    return 0;
+}
